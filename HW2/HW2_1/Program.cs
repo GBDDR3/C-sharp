8e061b102ad665ea0940ca1aab5ea7e234a2bfda
sergei <segal_005@rambler.ru>
2022-08-12 18:07:59 +0300

    HomeWork2_1 created

---
+++ HW2/HW2_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.WriteLine("Введите трёхзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+void FindNumber()
+{
+number = number %100;
+number = number / 10;
+Console.Write(number);
+}
+FindNumber();
